2fcb65a63848168b16a078b0f8af4ca3bc7d9c57
Piero Evangelista <qhd9zj@virginia.edu>
2026-02-09 22:24:46 -0500

    Cipher class

---
+++ src/main/java/Cipher.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cipher {
+    private static String decipher(String text, String key){
+        String[] lines = key.split("\\n", -1);
+        //validate cipher key
+        if (lines.length < 2){
+            throw new IllegalArgumentException("Key file must have 2 lines");
+        }
+        String firstLine = lines[0];
+        String secondLine = lines[1];
+        if (firstLine.length() != secondLine.length()){
+            throw new IllegalArgumentException("Key file must have equal length");
+        }
+        //building map
+        Map<Character, Character> decodeMap = new HashMap<>();
+        for (int i = 0; i < secondLine.length(); i++) {
+            decodeMap.put(secondLine.charAt(i), firstLine.charAt(i));
+        }
+        //decoding
+        StringBuilder decoded = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            decoded.append(decodeMap.getOrDefault(c, c));
+        }
+        //return decoded text
+        return decoded.toString();
+    }
+}
