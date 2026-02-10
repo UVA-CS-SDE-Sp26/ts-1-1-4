9c6eccc48a832b9b48f66b4c4facdfb5ddbbd071
Abel Saberhagen <abelsaberhagen@gmail.com>
2026-02-10 02:11:33 -0500

    file handler

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -2,4 +2,8 @@
  * Commmand Line Utility
  */
 public class TopSecret {
+    public static void main (String[] args) {
+        FileHandler topSecretFiles = new FileHandler();
+        topSecretFiles.readFile();
+    }
 }
