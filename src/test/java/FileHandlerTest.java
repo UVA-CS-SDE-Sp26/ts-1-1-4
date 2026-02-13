59f903911b085f24cad30d355007b9ab5504d3f5
Abel Saberhagen <abelsaberhagen@gmail.com>
2026-02-13 15:51:11 -0500

    tests

--- src/test/java/FileHandlerTest.java
+++ src/test/java/FileHandlerTest.java
@@ -8,50 +8,43 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class FileHandlerTest {
     @Mock
-    private FileHandler MockFileHandler;
-    private File MockFile;
+    private ProgramController mockProgramController;
+
+    private FileHandler testFileHandler;
+    public File testFile;
 
 
     @BeforeEach
     void setUp() {
-        MockFileHandler = mock(FileHandler.class);
+       mockProgramController = mock(ProgramController.class);
     }
 
-    @Test
-    @DisplayName ("filea.txt is a valid file.")
-    void testFileAExists() {
-        MockFile = new File("data/filea.txt");
-        assertTrue(MockFile.exists());
-    }
-
-    @Test
-    @DisplayName ("fileb.txt is a valid file.")
-    void testFileBExists() {
-        MockFile = new File("data/fileb.txt");
-        assertTrue(MockFile.exists());
-    }
-
-    @Test
-    @DisplayName ("text.txt is not a valid file.")
-    void testNotFileExists() {
-        MockFile = new File("text.txt");
-        assertFalse(MockFile.exists());
-    }
 
     @Test
     @DisplayName ("Correct content is read.")
     void testReadValidFile() throws Exception {
 
-        MockFileHandler.readFile("data/filea.txt");
-        String content = MockFileHandler.getData();
+        testFileHandler.readFile("data/filea.txt");
+        String content = testFileHandler.getData();
 
-        assertEquals("Expected content", content);
+        assertEquals("Hello World, this is ts-1-1-4.", content);
+    }
+
+    @Test
+    @DisplayName ("Test if file does not exist.")
+    void testExistence() {
+        testFileHandler = new FileHandler();
+
+        assertThrows(IOException.class, () -> {
+            testFileHandler.readFile("text.txt");
+        });
     }
 
 
